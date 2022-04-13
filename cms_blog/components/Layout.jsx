53093f228259a77d6b16915bd6baa07357435ffb
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-13 21:51:18 +0530

    changed on 13th April 2022

--- cms_blog/components/Layout.jsx
+++ cms_blog/components/Layout.jsx
@@ -1,13 +1,21 @@
-import React, { Children } from 'react'
+import React from 'react'
 import { Header } from './';
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
     return (
-       <>
-       <Header/>
-       {children}
-       </>
+        <>
+            <Header />
+            {children}
+        </>
     )
 }
+/*
+10 hrs 
+- web dev ke 5 projects finish
+- devops complete 
+- flutter ka half complete can complete.
+- web dev's backend complete can be done 
+- a lot of college portion can be completed.
+*/
 
 export default Layout
