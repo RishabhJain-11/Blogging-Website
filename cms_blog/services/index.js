6e64c3444a6fd52293711bf7828437557f3c2c7a
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-14 21:00:39 +0530

    changed on 14th April 2022

---
+++ cms_blog/services/index.js
@@ -0,0 +1,40 @@
+import {request ,gql} from 'graphql-request'
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHICS_ENDPOINT;
+
+export const getPost = async () => {
+
+    const query = gql`
+    query MyQuery {
+        postsConnection {
+          edges {
+            node {
+              author {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredimage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+      
+    `
+
+    const results = await request(graphqlAPI,query)
+
+}
