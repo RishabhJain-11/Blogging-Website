0a072bbd96a6182206659c3ff3c16f6bd9d7a34f
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-15 15:32:13 +0530

    changed on 15th April 2022

--- cms_blog/services/index.js
+++ cms_blog/services/index.js
@@ -39,4 +39,47 @@ export const getPost = async () => {
 
     return results.postsConnection.edges;
 
+};
+
+export const getRecentPosts = async () => {
+  const query = gql`
+      query GetPostDetails() {
+        posts(
+          orderBy: createdAt_ASC
+          last: 3
+        ) {
+          title 
+          featuredimage{
+            url
+          }
+          createdAt
+          slug
+        }
+      }
+  `
+
+  const result = await request(graphqlAPI,query);
+  return result.posts;
+}
+
+export const getSimilarPosts = async() => {
+
+  const query = gql`
+      query GetPostDetails($slug: String!, $categories:[String!]){
+        posts(
+          where:{slug_not:$slug,AND : {categories_some : {slug_in:$categories}}}
+          last:3
+        ) {
+          title 
+          featuredimage{
+            url
+          }
+          createdAt
+          slug
+        }
+      }
+  `
+
+  const result = await request(graphqlAPI,query);
+  return result.posts;
 }
